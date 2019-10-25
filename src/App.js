9d7b95ce969d7a31cb58d3caf5d5606f30f13ae2
shehan <shehanuf@gmail.com>
2019-10-24 20:25:02 -0400

    Added css and html

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Reactr
-        </a>
-      </header>
-    </div>
-  );
+class Counter extends Component{
+  constructor(){
+    super();
+    this.state={
+      count:0,
+    };
+
+  }
+  render(){
+    return(
+      <div className = "container">
+        <div className="navbar">Counter.js</div>
+        <div className="counter">
+          <h1> What do we put here </h1>
+          <button type="button"> Increment </button>
+          <button type="button"> Decrement </button>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default Counter;
